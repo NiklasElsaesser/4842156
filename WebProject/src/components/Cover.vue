f266de406e7ae14c212354ad350b8003aee7a906
NiEl20184 <83662773+NiEl20184@users.noreply.github.com>
2022-05-03 23:05:40 +0200

    cover done

--- WebProject/src/components/Cover.vue
+++ WebProject/src/components/Cover.vue
@@ -9,11 +9,18 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <!-- <h1 class="green">{{ msg }}</h1> -->
+    <h1>
+      Web Engineering Projekt
+    </h1>
     <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+      Klausurarbeit
+    </h3>
+    <h2>
+      von
+    </h2>
+    <h3>
+      Niklas Elsässer
     </h3>
   </div>
 </template>
