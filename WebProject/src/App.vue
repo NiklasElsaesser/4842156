0a44cbbbaadfbdc0c57a8bf7e37a4ba21554efe7
NiEl20184 <83662773+NiEl20184@users.noreply.github.com>
2022-05-03 21:13:28 +0200

    added contents + contentitem

--- WebProject/src/App.vue
+++ WebProject/src/App.vue
@@ -1,19 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Cover from './components/Cover.vue'
+import Contents from './components/Contents.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="DHBW-Logo" class="logo" src="./assets/DHBW-Logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Cover  />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <Contents />
   </main>
 </template>
 
