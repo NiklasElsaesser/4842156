7fb85a464f6509fcc7ed09ecb54f161903ed8a31
NiEl20184 <83662773+NiEl20184@users.noreply.github.com>
2022-05-04 23:01:54 +0200

    added vue

--- WebProject/src/main.js
+++ WebProject/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
 import Instagram from './components/Instagram.vue'
 
 createApp(App).mount('#app')
 createApp(Instagram).mount('#instagram')
+
