fdb5b1fd561a4b07fcdb815302173226a02dfd02
NiEl20184 <83662773+NiEl20184@users.noreply.github.com>
2022-05-30 22:38:20 +0200

    initial commit

--- WebProject/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
-import Instagram from './components/Instagram.vue'
-
-createApp(App).mount('#app')
-createApp(Instagram).mount('#instagram')
-
